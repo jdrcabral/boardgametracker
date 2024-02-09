6868f36c0bd3eccee1180c0982ec48130c8eefb8
Joao Cabral <joao.cabral@complyadvantage.com>
2024-02-09 15:55:40 +0000

    feat: fix firefox tests

--- tests/resident_evil/resident_evil_map.spec.js
+++ tests/resident_evil/resident_evil_map.spec.js
@@ -4,15 +4,15 @@ const { test, expect } = require('@playwright/test');
 test('should reveal map', async ({ page }) => {
   await page.goto('http://localhost:3000/pages/boardgame/residentevil.html');
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await expect(page.locator('#rect36')).toBeVisible();
+  await expect(page.locator('xpath=//*[@id="1st_Floor_East_A"]')).toBeVisible();
   await expect(page.locator('xpath=/html/body/div/div[4]/div[4]/svg/g/g[3]/g[16]/path')).not.toBeVisible();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await expect(page.locator('#modalTitle')).toContainText('1st Floor East A');
   await page.getByRole('button', { name: 'Reveal ' }).click();
   await expect(page.locator('#rect36')).toBeVisible();
   await page.reload();
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await expect(page.locator('#rect36')).toBeVisible();
   await expect(page.locator('#path98')).toBeVisible();
 });
@@ -20,9 +20,9 @@ test('should reveal map', async ({ page }) => {
 test('should hide a revealed map', async ({ page }) => {
   await page.goto('http://localhost:3000/pages/boardgame/residentevil.html');
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await expect(page.locator('#rect36')).toBeVisible();
+  await expect(page.locator('xpath=//*[@id="1st_Floor_East_A"]')).toBeVisible();
   await expect(page.locator('xpath=/html/body/div/div[4]/div[4]/svg/g/g[3]/g[16]/path')).not.toBeVisible();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await expect(page.locator('#modalTitle')).toContainText('1st Floor East A');
   await page.getByRole('button', { name: 'Reveal ' }).click();
   await expect(page.locator('#rect36')).toBeVisible();
@@ -30,21 +30,21 @@ test('should hide a revealed map', async ({ page }) => {
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
   await expect(page.locator('#rect36')).toBeVisible();
   await expect(page.locator('#path98')).toBeVisible();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await expect(page.locator('#modalTitle')).toContainText('1st Floor East A');
   await page.getByRole('button', { name: 'Reveal ' }).click();
-  await expect(page.locator('#rect36')).toBeVisible();
+  await expect(page.locator('xpath=//*[@id="1st_Floor_East_A"]')).toBeVisible();
   await expect(page.locator('xpath=/html/body/div/div[4]/div[4]/svg/g/g[3]/g[16]/path')).not.toBeVisible();
   await page.reload();
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await expect(page.locator('#rect36')).toBeVisible();
+  await expect(page.locator('xpath=//*[@id="1st_Floor_East_A"]')).toBeVisible();
   await expect(page.locator('xpath=/html/body/div/div[4]/div[4]/svg/g/g[3]/g[16]/path')).not.toBeVisible();
 });
 
 test('should complete a scenario', async ({ page }) => {
   await page.goto('http://localhost:3000/pages/boardgame/residentevil.html');
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await page.getByRole('button', { name: 'Complete ' }).click();
   await expect(page.locator('#rect5')).toHaveAttribute('fill', '#2dcf43');
   await page.reload();
@@ -55,13 +55,13 @@ test('should complete a scenario', async ({ page }) => {
 test('should reverse a complete scenario', async ({ page }) => {
   await page.goto('http://localhost:3000/pages/boardgame/residentevil.html');
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await page.getByRole('button', { name: 'Complete ' }).click();
   await expect(page.locator('#rect5')).toHaveAttribute('fill', '#2dcf43');
   await page.reload();
   await page.getByRole('tab', { name: 'Mansion Map' }).click();
   await expect(page.locator('#rect5')).toHaveAttribute('fill', '#2dcf43');
-  await page.locator('#tspan175').click();
+  await page.locator('xpath=//*[@id="1st_Floor_East_A"]').click();
   await page.getByRole('button', { name: 'Complete ' }).click();
   await expect(page.locator('#rect5')).toHaveAttribute('fill', '#ffffff');
   await page.reload();
