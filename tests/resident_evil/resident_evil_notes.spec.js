136a1726d3216f661092f9b07e8ebb4a108ae772
Joao Cabral <joao.cabral@complyadvantage.com>
2024-02-23 16:56:57 +0000

    fix: notes being replaced by undefined

--- tests/resident_evil/resident_evil_notes.spec.js
+++ tests/resident_evil/resident_evil_notes.spec.js
@@ -10,3 +10,22 @@ test('should store note', async ({ page }) => {
   await page.reload()
   await expect(page.locator('xpath=//*[@id="gameNotes"]')).toHaveValue('Test')
 })
+
+test('should keep notes when change campaign', async ({ page }) => {
+  await page.goto('http://localhost:3000/pages/boardgame/residentevil.html')
+  await page.getByLabel('Notes').click()
+  await page.getByLabel('Notes').fill('Test')
+  await page.getByLabel('Notes').press('Tab');
+  await expect(page.getByLabel('Notes')).toHaveValue('Test')
+  await page.reload()
+  await expect(page.locator('xpath=//*[@id="gameNotes"]')).toHaveValue('Test')
+  await page.getByRole('button', { name: 'New Campaign' }).click();
+  await page.getByLabel('Notes').click();
+  await page.getByLabel('Notes').fill('New Campaign');
+  await page.getByLabel('Notes').press('Tab');
+  await page.reload()
+  await page.getByLabel('Campaign Select').selectOption('New Campaign');
+  await expect(page.getByLabel('Notes')).toHaveValue('New Campaign');
+  await page.getByLabel('Campaign Select').selectOption('Game Campaign');
+  await expect(page.getByLabel('Notes')).toHaveValue('Test');
+})
