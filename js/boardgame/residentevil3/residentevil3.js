a27d07ab97d0e014fc39a377842b72aa15088cc6
Joao Cabral <joao.cabral@complyadvantage.com>
2024-05-03 10:26:57 +0100

    feat: broken test

--- js/boardgame/residentevil3/residentevil3.js
+++ js/boardgame/residentevil3/residentevil3.js
@@ -59,7 +59,7 @@ function loadCard (containerId, element, backgroundColor = null, includeQuantity
   const container = document.getElementById(containerId)
   const cardElement = buildCard(element, includeQuantity, quantity, inputType)
   if (backgroundColor) {
-    cardElement.style.backgroundColor = backgroundColor
+    cardElement.setBackgroundColor(backgroundColor);
   }
   const colDiv = ComponentCreator.createDivWithClass('col-xs-12 col-md-3 mb-3', [cardElement.build()])
   container.appendChild(colDiv)
